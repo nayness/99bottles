7d75650c8807ddd8bcb99b18c21f346c5c5f72da
Nayness <nayness@gmail.com>
2017-07-09 22:34:12 -0400

    test_verse_1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,6 +12,9 @@ class Bottles
 		when 2
 			"2 bottles of beer on the wall, 2 bottles of beer.\n" +
 			"Take one down and pass it around, 1 bottle of beer on the wall.\n"
+		when 1
+			"1 bottle of beer on the wall, 1 bottle of beer.\n" +
+			"Take it down and pass it around, no more bottles of beer on the wall.\n"
 		end
 	end
 end
