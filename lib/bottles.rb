be9cd4989c6a3bcf1ed469258316a7f1489488c3
Nayness <nayness@gmail.com>
2017-07-09 22:25:16 -0400

    test_the_first_verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,11 @@
 class Bottles
 
 	def verse(n)
+
+		case n
+		when 99
+			"99 bottles of beer on the wall, 99 bottles of beer.\n" +
+			"Take one down and pass it around, 98 bottles of beer on the wall.\n"
+		end
 	end
 end
