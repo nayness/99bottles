c34024f91bb5b9e73797373df60886f4c2fb0eab
Nayness <nayness@gmail.com>
2017-07-09 22:43:51 -0400

    test_a_couple_verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,21 +3,23 @@ class Bottles
 	def verse(n)
 
 		case n
-		when 99
-			"99 bottles of beer on the wall, 99 bottles of beer.\n" +
-			"Take one down and pass it around, 98 bottles of beer on the wall.\n"
-		when 89
-			"89 bottles of beer on the wall, 89 bottles of beer.\n" +
-			"Take one down and pass it around, 88 bottles of beer on the wall.\n"
-		when 2
-			"2 bottles of beer on the wall, 2 bottles of beer.\n" +
-			"Take one down and pass it around, 1 bottle of beer on the wall.\n"
-		when 1
-			"1 bottle of beer on the wall, 1 bottle of beer.\n" +
-			"Take it down and pass it around, no more bottles of beer on the wall.\n"
+
 		when 0
 			"No more bottles of beer on the wall, no more bottles of beer.\n" +
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+		when 1
+			"1 bottle of beer on the wall, 1 bottle of beer.\n" +
+			"Take it down and pass it around, no more bottles of beer on the wall.\n"
+		when 2
+			"2 bottles of beer on the wall, 2 bottles of beer.\n" +
+			"Take one down and pass it around, 1 bottle of beer on the wall.\n"
+		else
+			"#{n} bottles of beer on the wall, #{n} bottles of beer.\n" +
+			"Take one down and pass it around, #{n-1} bottles of beer on the wall.\n"
 		end
+	end
+
+	def verses(first, last)
+		first.downto(last).collect {|bottle| verse(bottle)}.join("\n")
 	end
 end
