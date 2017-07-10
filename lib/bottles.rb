b9739eafd1f1e0f97e805229b4a0d1acea57c0ab
Nayness <nayness@gmail.com>
2017-07-09 22:36:10 -0400

    test_verse_0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,6 +15,9 @@ class Bottles
 		when 1
 			"1 bottle of beer on the wall, 1 bottle of beer.\n" +
 			"Take it down and pass it around, no more bottles of beer on the wall.\n"
+		when 0
+			"No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 		end
 	end
 end
